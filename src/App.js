ee614591e2eda2d70fb2935037df40d23ca27777
miss-mad <mmlh.harris@gmail.com>
2023-05-02 21:22:56 -0400

    adding comments and useState to handle input box change for added tasks

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import ToDo from "./components/ToDo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
-function App(props) {
+function App({ tasks }) {
+  // passing props from child (Form) to parent (App) by creating a function in parent (App) that expects data and passing that function to the child (Form)
+  // also called a callback prop (a function as a prop)
   const addTask = (taskName) => alert(taskName);
-  const taskList = props.tasks.map((task) => (
+
+  // map over the data from index.js
+  const taskList = tasks.map((task) => (
     <ToDo
       id={task.id}
       name={task.name}
@@ -13,9 +17,11 @@ function App(props) {
       key={task.id}
     />
   ));
+
   return (
     <div className="todoapp stack-large">
       <h1>To Do or Not To Do</h1>
+      {/* passing the task as prop to the child (Form) so we can obtain the data (new task name) */}
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Form({ addTask }) {
+  // useState to capture the new task in the form input
+  const [name, setName] = useState("");
+
+  // sends the addTask back to the parent (App) to add the new task to the list
+  // at this point, if you type something in the input box and click "add", an alert will fire from the parent (App) with the text that you just typed, letting you know that addTask is successfully going back to the parent after being passed to the child (Form) as a prop
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTask("Say hello!");
+    addTask(name);
+    // clear input
+    setName("");
+  };
+
+  // find and set the value of the new task name in the form input
+  const handleInputChange = (e) => {
+    console.log(e.target.value);
+    setName(e.target.value);
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -18,6 +31,8 @@ export default function Form({ addTask }) {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleInputChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
